7cd70b1cbf78c98f3948444484ee929000053655
Rik <Rik@rik-PC.Safeware.local>
2011-11-16 13:58:26 +0000

    New

---
+++ HS.Algorithms.Tests/BaseNCodecFixture.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+using NUnit.Framework;
+
+namespace Safeware.Utils.Tests
+{
+    [TestFixture]
+    public class BaseNCodecFifture
+    {            
+        [Test]
+        public void RoundTrips()
+        {
+            const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            foreach (var n in new[] { 0, 1, 2, 42, 1024, BigInteger.Parse("4125636888562548868221559797461449"), BigInteger.Parse("47023505233334520000") })
+                Assert.AreEqual(n, BaseNCodec.Decode(BaseNCodec.Encode(n, alphabet), alphabet));
+        }
+
+        [Test]
+        public void RoundTripsPleasantUri()
+        {
+            const string alphabet = "BCDGHJKLMPQRSTVWXYZbcdghjklmpqrstvwxyz0123456789_-";
+
+            foreach (var n in new[] { 0, 1, 2, 42, 1024, BigInteger.Parse("4125636888562548868221559797461449"), BigInteger.Parse("47023505233334520000") })
+                Assert.AreEqual(n, BaseNCodec.Decode(BaseNCodec.Encode(n, alphabet), alphabet));
+        }
+
+        [Test]
+        public void Prettiness()
+        {
+            const string alphabet = "BCDGHJKLMPQRSTVWfXZbcdghjklmpqrstvwxyz0123456789";
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var guid = Guid.NewGuid();
+
+                var n = BigInteger.Parse("0" + guid.ToString("N"), NumberStyles.HexNumber);
+
+                var s = BaseNCodec.Encode(n, alphabet);
+
+                Console.WriteLine("http://libry.net/" + s.PadLeft(23, '_') + ".pdf\t->\t" + "http://libry.net/" + guid.ToString("N") + ".pdf");
+            }
+        }
+    }
+}
---
+++ HS.Algorithms/BaseNCodec.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace Safeware.Utils
+{
+    public static class BaseNCodec
+    {
+        public static string Encode(BigInteger bigNumber, string alphabet)
+        {
+            var ret = new StringBuilder();
+
+            if (bigNumber == 0)
+                return alphabet[0].ToString();
+
+            BigInteger theBase = alphabet.Length;
+
+            while (bigNumber.CompareTo(0) != 0)
+            {
+                BigInteger remainder;
+                var newBigNumber = BigInteger.DivRem(bigNumber, theBase, out remainder);
+                ret.Insert(0, alphabet[(int)remainder]);
+                bigNumber = newBigNumber;
+            }
+
+            return ret.ToString();
+        }
+
+        public static BigInteger Decode(string data, string alphabet)
+        {
+            var theBase = alphabet.Length;
+            var dataLength = data.Length;
+
+            var i = 0;
+
+            return data.Aggregate<char, BigInteger>
+                (0,
+                 (current, c) =>
+                 current + BigInteger.Multiply(alphabet.IndexOf(c), BigInteger.Pow(theBase, dataLength - (i++ + 1)))
+                );
+        }
+    }
+}
